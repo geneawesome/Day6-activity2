2d7e8d78245413480e9202d8b57270f920483c2f
geneawesome <genecolumna@gmail.com>
2023-09-05 15:14:39 +0800

    new

---
+++ DAY 1/index.js
@@ -0,0 +1,40 @@
+let books = {
+    authorName: "Eugene",
+    Title: "book",
+    isAvailable: true,
+    isNotAvailable: false,
+
+
+
+start: function() {
+    console.log("Eugene");
+    return false;
+}
+};
+console.log(books.start());
+
+
+
+
+
+
+
+
+
+
+// activity 2
+// let car = {
+//     volvo: "Eugene",
+//         price: "4500",
+//         isAutomatic: true,
+//         isManual: false,
+    
+    
+    
+//     start: function() {
+//         console.log("Volvo");
+//         console.log("price");
+//     }
+//     };
+//     console.log(car.start());
+
